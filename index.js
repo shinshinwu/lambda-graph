4838a9300600d63f08ab7b5af1d9ea653da739c8
Anna Wu <shinshinwu@gmail.com>
2018-10-26 17:51:05 -0700

    drawing stacked chart success

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 var fs = require('fs')
 var gm = require('gm').subClass({ imageMagick: true });
 
-gm(200, 100, '#fff')
-.drawText(10, 20, "hello world")
-.drawRectangle(50,10,80,60)
-.border(2, 2).borderColor('#ff0000')
+// gm("img.png").drawRectangle(x0, y0, x1, y1 [, wc, hc])
+// .drawText(10, 20, "hello world")
+
+// @datasets = [
+//   [:Jimmy, [25, 36, 86, 39]],
+//   [:Charles, [80, 54, 67, 54]],
+//   [:Julie, [22, 29, 35, 38]],
+//   ]
+
+gm(200, 100, 'transparent')
+.fill('#49669f')
+.drawRectangle(10,100,50,90)
+.drawRectangle(50,100,90,80)
+.fill('#f3ddcf')
+.drawRectangle(10,90,50,70)
+.drawRectangle(50,80,90,65)
+.fill('#992f2f')
+.drawRectangle(10,70,50,53)
+.drawRectangle(50,65,90,50)
 .write('/Users/anna/Documents/webdev/eva-lambda/charts/test.png', function (err) {
   if (!err) console.log('done');
   if (err) console.log(err);
