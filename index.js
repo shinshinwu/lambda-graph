ca939e60cdf690c19b8a4075576e87d5dbe6a42c
Anna Wu <shinshinwu@gmail.com>
2018-10-28 21:12:27 -0700

    figured out how to draw chart using similar data to actual, now need to string everything together

--- index.js
+++ index.js
@@ -7,17 +7,12 @@ const axios = require('axios');
 // gm("img.png").drawRectangle(x0, y0, x1, y1 [, wc, hc])
 // .drawText(10, 20, "hello world")
 
-// @datasets = [
-//   [:Jimmy, [25, 36, 86, 39]],
-//   [:Charles, [80, 54, 67, 54]],
-//   [:Julie, [22, 29, 35, 38]],
-//   ]
 
 // exports.handler = function(event, context, callback) {
 
   // let _callback = callback;
 
-  const url = "https://dailygieselmann.com/time-series/eva%3Aoptinmonster.com?raw"
+  // const url = "https://dailygieselmann.com/time-series/eva%3Aoptinmonster.com?raw"
 
   // axios.get(url)
   // .then(response => {
@@ -34,20 +29,27 @@ const axios = require('axios');
   //   console.log(error);
   // });
 
-  // var data = [
-  //   {points: [25, 36, 86, 39], color: '#49669f'},
-  //   {points: [80, 54, 67, 54], color: '#f3ddcf'},
-  //   {points: [22, 29, 35, 38], color: '#992f2f'}
-  // ]
-
   // TODO: need to parse the data to below format and think about how to distribute the width and height pixels
+  // TODO: Need to test this with single band charts
+  // TODO: make good comments for the code and include in the eva code base
 
-  colors = ['#49669f', '#f3ddcf', '#992f2f']
+  const evaRankColors = [
+    {start: 4500000, end: 3000000, color: '#344b75', divider: 150, order: 10},
+    {start: 3000000, end: 2000000, color: '#4966A0', divider: 100, order: 9},
+    {start: 2000000, end: 1000000, color: '#638ABB', divider: 100, order: 8},
+    {start: 1000000, end: 500000,  color: '#92C7E3', divider: 50,  order: 7},
+    {start: 500000,  end: 200000,  color: '#D0F2FB', divider: 30,  order: 6},
+    {start: 200000,  end: 100000,  color: '#F3DDCF', divider: 10,  order: 5},
+    {start: 100000,  end: 50000,   color: '#D99C7F', divider: 5,   order: 4},
+    {start: 50000,   end: 25000,   color: '#BC614C', divider: 2.5, order: 3},
+    {start: 25000,   end: 10000,   color: '#992F2F', divider: 1.5, order: 2},
+    {start: 10000,   end: 1,       color: '#60151F', divider: 1,   order: 1}
+  ];
 
-  let data = [
-    [10, 20, 17],
-    [20, 15, 15]
-  ]
+  // let data = [
+  //   [10, 20, 17],
+  //   [20, 15, 15]
+  // ]
 
   // (10,100,50,90)
   // (10,90,50,70)
@@ -56,25 +58,93 @@ const axios = require('axios');
   // (50,80,90,65)
   // (50,65,90,50)
 
+  let data = [104213, 89953, 36812, 24863, 20893, 19513, 30257, 18854, 7660, 6716]
+
+  let trafficMax = Math.max(...data) // largest number for traffic, lowest ranking
+  let trafficMin = Math.min(...data) // lowest number for traffic, highest ranking
+
+  let startBand = evaRankColors.find((v) => {
+    if ((v.start >= trafficMax) && (v.end < trafficMax)) {
+      return true
+    }
+  })
+
+  let endBand = evaRankColors.find((v) => {
+    if ((v.start >= trafficMin) && (v.end < trafficMin)) {
+      return true
+    }
+  })
+
   let output = []
 
-  let width = 900
-  let height = 300
+  let padding = 10 // leave 10 padding on all sides except for bottom
+  let xIncrement = 50
+
+  let formattedData = []
+  let bands = []
+  let orderedColors = []
+
+  evaRankColors.forEach((v) => {
+    if ((v.order <= startBand.order) && (v.order >= endBand.order)) {
+      bands.push(v)
+    }
+  })
+
+  bands.forEach(band => {
+    orderedColors.push(band.color)
+    formattedData.push({bandOrder: band.order, chartData:[]})
+  })
+
+  console.log('colors are ', orderedColors)
+
+  data.forEach((point, trafficIndex) => {
+    bands.forEach((band, bandIndex) => {
+      // if the current ranking is less (higher) than the band start, fill the color, otherwise the ranking does not fill this current band
+      let dataBand = formattedData.find(d => d.bandOrder == band.order)
+
+      if (point <= band.start) {
+        let bandDiff = band.start - point
+        let bandSize = band.start - band.end
+
+        // if the ranking ends in this band, only fill the diff, otherwise fill the entire band
+        if (bandDiff <= bandSize) {
+          dataBand.chartData.push(bandDiff / band.divider)
+        } else {
+          dataBand.chartData.push(bandSize / band.divider)
+        }
+      } else {
+        dataBand.chartData.push(0)
+      }
+    })
+  })
+
+  // transpose array
+  let transposedData = []
+
+  formattedData.forEach(e => transposedData.push(e.chartData))
+
+  transposedData = transposedData[0].map((col, i) => transposedData.map(row => row[i]));
+
+  console.log('transposed array is ', transposedData)
+
+  let width = transposedData.length * xIncrement + padding * 2
+  // height equals the max value of the highet bar (highest sum of transposed array)
+  let height = Math.max(...transposedData.map(a => {return a.reduce((sum, v) => sum+v)})) + padding
+
+  console.log('chart width and height are ', width, height)
+
   let chart = gm(width, height, 'transparent')
 
-  let xInitialStart = 10
-  let xIncrement = 40
-
-  data.forEach((v, index) => {
-    let xStart = xInitialStart + xIncrement * index
-    let xEnd = xInitialStart + xIncrement * (index + 1)
+  transposedData.forEach((v, index) => {
+    let xStart = padding + xIncrement * index
+    let xEnd = padding + xIncrement * (index + 1)
     let yStart = height
 
     v.forEach((point, colorIndex) => {
       let yEnd = yStart - point
-      chart.fill(colors[colorIndex]);
+      chart.fill(orderedColors[colorIndex]);
       chart.drawRectangle(xStart, yStart, xEnd, yEnd)
-      // output.push({coors: [xStart, yStart, xEnd, yEnd], color: colors[colorIndex]})
+      output.push({coors: [xStart, yStart, xEnd, yEnd], color: orderedColors[colorIndex]})
       yStart = yEnd
     })
 
@@ -86,6 +156,8 @@ const axios = require('axios');
     //   chart.drawRectangle(xStart,yStart,xEnd,yEnd)
     // })
   })
+
+  console.log(output)
 
 
   // chart.fill('#49669f')
@@ -100,7 +172,10 @@ const axios = require('axios');
   // .drawRectangle(10,70,50,53)
   // .drawRectangle(50,65,90,50)
 
-  chart.write('/Users/anna/Documents/webdev/eva-lambda/charts/test.png', function (err) {
+  // resize the image and force to change the aspect ratio (https://aheckmann.github.io/gm/docs.html#drawing-primitives)
+
+  chart.resize(900, 300, "!")
+    .write('/Users/anna/Documents/webdev/eva-lambda/charts/test.png', function (err) {
     if (!err) console.log('done');
     if (err) console.log(err);
   });
